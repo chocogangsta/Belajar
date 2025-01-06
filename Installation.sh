f166dc5e9fec03c673d202e769f2bd64bbf5c7f5
chocogangsta <138999111+chocogangsta@users.noreply.github.com>
2025-01-06 15:28:12 +0700

    Update Installation.sh

--- Installation.sh
+++ Installation.sh
@@ -2,6 +2,8 @@
 
 # Unduh installer Miniconda
 sudo wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /opt/miniconda-installer.sh
+source ~/miniconda3/bin/activate
+conda init --all
 
 # Jalankan installer
 sudo bash /opt/miniconda-installer.sh
